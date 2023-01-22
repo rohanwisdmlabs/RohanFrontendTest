5d40fa4d46e8e1ceda24abceb46378b76c1d5795
rohanwisdmlabs <rohan.ray@wisdmlabs.com>
2023-01-22 23:19:36 +0530

    made the second-section

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".card-carousel").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: false,
+    infinite: true,
+    variableWidth: true,
+  });
+});
